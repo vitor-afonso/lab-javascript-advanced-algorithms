936ea6de9bc211716cd2a6423daf54211bf1c86f
Vitor Afonso <vittokm@hotmail.com>
2021-11-30 20:08:48 +0000

    stuck in the renderListStack on file --> stack-dom.js

--- js/stack/stack-dom.js
+++ js/stack/stack-dom.js
@@ -1,46 +1,74 @@
+/*jshint esversion:6 */
+
+
 const stackList = document.getElementById('stack-list');
 const stackInput = document.getElementById('stack-input');
 const container = document.getElementById('container');
 const warningTopStack = document.querySelector('#stack-container .warning-top');
-const warningBottomStack = document.querySelector(
-  '#stack-container .warning-bottom'
-);
+const warningBottomStack = document.querySelector('#stack-container .warning-bottom');
 const addStackBtn = document.getElementById('add-stack');
 const takeStackBtn = document.getElementById('take-stack');
 
 const newStack = new Stack();
 
 const clearStackInput = () => {
-  // ... your code goes here
+  stackInput.value = "";
 };
 
+
+
+// FIX THE RENDER THAT IS ADDIND THE ALL ARRAY AGAIN
 const renderListStack = () => {
-  // ... your code goes here
+
+  newStack.display().forEach(element => {
+
+    let newLi = document.createElement('li');
+    newLi.innerHTML = element;
+    stackList.appendChild(newLi);
+    
+  });
 };
 
 renderListStack();
 
 const generateWarningStack = (type) => {
-  if (type === 'underflow') {
-    // ... your code goes here
-  } else if (type === 'overflow') {
-    // ... your code goes here
+  if (type === 'STACK_UNDERFLOW') {
+    
+    
+  } else if (type === 'STACK_OVERFLOW') {
+    warningTopStack.textContent = type;
   }
 };
 
 const addToStack = () => {
   try {
-    // ... your code goes here
+
+    if (!stackInput.value) {
+      return;
+    }
+
+    newStack.stackControl.push(stackInput.value);
+
+    clearStackInput();
+    renderListStack();
+
   } catch (error) {
-    // there was an overflow error, handle it
+    
+    error.type = 'STACK_OVERFLOW';
+    generateWarningStack(error.type);
+    
   }
+  
 };
 
 const removeFromStack = () => {
   try {
-    // ... your code goes here
+
+    newStack.stackControl.pop();
   } catch (error) {
-    // there was an underflow error, handle it
+
+    error.type = 'STACK_OVERFLOW';
+    generateWarningStack(error.type);
   }
 };
 
